c8538380c48d3d663f010dd9b3ed000a7c2e6c8e
YuklidD <ict21895@sjp.ac.lk>
2024-01-29 19:52:12 +0530

    Break component

---
+++ src/Pages/home/NevigationBar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Container, Navbar, Nav, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const NavigationBar = () => {
+  return (
+    <Navbar bg="primary" variant="dark" expand="lg" fixed="top" className="navbar">
+      <Container>
+        <Navbar.Brand href="#home">Bus Schedule</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          <Link to="/login" className="nav-link">
+            <Button variant="outline-light" className="ms-lg-auto">
+              Login
+            </Button>
+          </Link>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavigationBar;
---
+++ src/Pages/home/ReservationForm.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Form, Button, Row, Col, InputGroup, FormControl } from 'react-bootstrap';
+
+const ReservationForm = () => {
+  return (
+    <>
+    <Form>
+      <h5 className="destination mb-3">Destination</h5>
+      <Row>
+        <Col xs={12} md={6}>
+          <InputGroup className="mb-3">
+            <FormControl placeholder="From" aria-label="From" />
+            <Button variant="outline-secondary" id="button-addon1">
+              Select
+            </Button>
+          </InputGroup>
+        </Col>
+        <Col xs={12} md={6}>
+          <InputGroup className="mb-3">
+            <FormControl placeholder="To" aria-label="To" />
+            <Button variant="outline-secondary" id="button-addon2">
+              Select
+            </Button>
+          </InputGroup>
+        </Col>
+      </Row>
+
+      <h5 className="dateTime mb-3">Date & Time</h5>
+      <Row>
+        <Col xs={12} sm={6}>
+          <FormControl type="date" placeholder="Date" aria-label="Date" className="mb-3" />
+        </Col>
+        <Col xs={12} sm={6}>
+          <FormControl type="time" placeholder="Time" aria-label="Time" />
+        </Col>
+      </Row>
+
+      <Button variant="primary" size="lg" className="mb-4 w-100">
+        Check
+      </Button>
+    </Form>
+
+    <div className="areYouASection">
+          <h5 className="areYouA mb-3">Are you a bus owner?</h5>  
+          <Button variant="outline-primary" size="lg" className="w-100">
+            Add your bus
+          </Button>
+        </div>
+    </>
+  );
+};
+
+export default ReservationForm;
