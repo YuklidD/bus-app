2714fefe90943f7eba626c76906113b4d0ddcd82
YuklidD <ict21895@sjp.ac.lk>
2024-01-29 20:33:20 +0530

    1

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -2,7 +2,6 @@
 
 import React from 'react';
 import { Container } from 'react-bootstrap';
-import {Button} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Home.css';
 import NavigationBar from './NevigationBar';
