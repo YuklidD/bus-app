d7b34ff3f4035db9108139eccc26f4a006f10065
YuklidD <ict21895@sjp.ac.lk>
2024-03-12 15:14:03 +0530

    Package.json updated for Host

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -29,36 +29,35 @@ const Home = () => {
     <Container fluid className="p-0 home">
       <NavigationBar />
       {/* Row for reservation form, GIF, and image content */}
-      <Row className="mt-5 pt-4 align-items-start">
+      <Row className="mt-5 pt-4 align-items-start gx-3"> {/* Adjust the gap with gx-* class */}
         <Col md={6} className="reservation-column">
-          {/* Reservation Form and GIF */}
+          {/* Reservation Form and Lottie Animation */}
           <ReservationForm />
-          {/* Place the GIF right under the Reservation Form */}
           <div className="lottie-animation">
-          <Lottie options={defaultOptions} height={400} width={400} />
+            <Lottie options={defaultOptions} height={400} width={400} />
           </div>
         </Col>
         <Col md={6} className="content-column">
-          {/* Right column text content extracted from the image provided */}
+          {/* Right column text content */}
           <div className="detail-content">
             <h1>Your seamless journey starts here.</h1>
             <p>Reserve your bus seat hassle-free.</p>
             <div className="button-group">
-            <a href="#" className="badge bg-primary rounded-pill p-2 me-1 btn-badge">
-            <img src={FindIcon} alt="Find" className="icon-class" />Find
-          </a>
-          <a href="#" className="badge bg-secondary rounded-pill p-2 me-1 btn-badge">
-            <img src={SelectIcon} alt="Select" className="icon-class" />Select
-          </a>
-          <a href="#" className="badge bg-success rounded-pill p-2 btn-badge">
-            <img src={ReserveIcon} alt="Reserve" className="icon-class" />Reserve
-          </a>
-          </div>
-
+              <a href="#" className="badge bg-primary rounded-pill p-2 me-1 btn-badge">
+                <img src={FindIcon} alt="Find" className="icon-class" />Find
+              </a>
+              <a href="#" className="badge bg-secondary rounded-pill p-2 me-1 btn-badge">
+                <img src={SelectIcon} alt="Select" className="icon-class" />Select
+              </a>
+              <a href="#" className="badge bg-success rounded-pill p-2 btn-badge">
+                <img src={ReserveIcon} alt="Reserve" className="icon-class" />Reserve
+              </a>
+            </div>
             <p className='p1'>Experience the effortless way to book your bus seats with us. Whether you're planning a quick getaway or a cross-country adventure, our convenient service puts you in control of your journey, every step of the way.</p>
           </div>
         </Col>
       </Row>
+
       <Footer />
     </Container>
   );
